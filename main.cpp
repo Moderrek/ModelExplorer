dff4a73354641076166150f40b3858d4ca364757
Moderr <tymonek12345@gmail.com>
2023-11-12 10:39:34 +0100

    Dodaj pliki projektów.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+	glfwInit();
+	// Using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	// Create GLFWwindow object of 800 by 800 pixels, naming it "Model Explorer"
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Model Explorer", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
+	// Load GLAD  so it configures OpenGL
+	gladLoadGL();
+	// Specify viewport of OpenGL in the Window
+	// x = 0, y = 0 to x = 800, y = 800
+	glViewport(0, 0, 800, 800);
+	// Specify the color of the background
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// Clean the back buffer and assign the new color of it
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap the back buffer with the front buffer
+	glfwSwapBuffers(window);
+	// Main while loop
+	while (!glfwWindowShouldClose(window)) {
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+	// Delete window
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
